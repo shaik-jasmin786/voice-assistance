cedb6bf0b7a820ec73376b85e6ae7dc3f447a35b
shaik-jasmin786 <jasminshaik502@gmail.com>
2025-07-20 21:58:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+'''import tkinter as tk
+from gui.app_gui import VoiceAssistantGUI
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = VoiceAssistantGUI(root)
+    root.mainloop()'''
+    
+# main.py
+
+import tkinter as tk
+from gui.app_gui import VoiceAssistantGUI
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = VoiceAssistantGUI(root)
+    root.mainloop()
+
